1917fad7650d14b135d13eb54f67c1dad82d76c0
Adrian de Freitas <Adefreit@users.noreply.github.com>
2022-12-26 18:08:05 -0700

    Uploading Template

---
+++ listAsDoubleLinkedList.c
@@ -0,0 +1,9 @@
+/** 
+ * ================================================================
+ * Name: 
+ * Section: 
+ * Project: Implementation of Double-Linked List Library
+ * =================================================================
+ */
+#include "listAsDoubleLinkedList.h"
+
---
+++ listAsDoubleLinkedListUnitTests.c
@@ -0,0 +1,20 @@
+/** 
+ * ===========================================================
+ * Name: 
+ * Section: 
+ * Project: 
+ * Purpose: List ADT as linked list
+ * ===========================================================
+ */
+
+int main() {
+
+    // Insert an appropriate test harness for your linked list implementation.  
+    // Your tests will test each function when the list is empty, has a single element, and will also
+    // test modifications to the head, tail, or middle elements.
+
+    // You are not being judged/graded on the quality of your lab
+    // However, it is in your best interest to make a high quality ADT for future assignments . . . hint hint
+
+    return 0;
+}
