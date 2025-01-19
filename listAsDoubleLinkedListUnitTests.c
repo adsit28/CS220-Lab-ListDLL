a1fef4c3b3ce0e7797147d92a088b715b180345f
troywein <troywein@gmail.com>
2025-01-19 13:02:48 -0700

    s25

--- listAsDoubleLinkedListUnitTests.c
+++ listAsDoubleLinkedListUnitTests.c
@@ -13,8 +13,5 @@ int main() {
     // Your tests will test each function when the list is empty, has a single element, and will also
     // test modifications to the head, tail, or middle elements.
 
-    // You are not being judged/graded on the quality of your lab
-    // However, it is in your best interest to make a high quality ADT for future assignments . . . hint hint
-
     return 0;
 }
